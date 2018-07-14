f0acec8d33b10c28f6098a409627925634830b27
João Vitor <joao.vitor.oliveira963@gmail.com>
2018-07-14 01:51:05 -0300

    Adicionado anotacoes para desabilitar autoconfiguracao de algumas classes

--- src/main/java/com/joaooliveira/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/joaooliveira/pontointeligente/api/PontoInteligenteApplication.java
@@ -1,8 +1,18 @@
 package com.joaooliveira.pontointeligente.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.jms.JndiConnectionFactoryAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
+@EnableAutoConfiguration(exclude = {JndiConnectionFactoryAutoConfiguration.class,DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,JpaRepositoriesAutoConfiguration.class,DataSourceTransactionManagerAutoConfiguration.class})
+@ComponentScan
 @SpringBootApplication
 public class PontoInteligenteApplication {
 
