02fbec00ba3e1824fa4e7430c9c96ba0748cb77b
João Vitor Oliveira <joao.oliveira@saqueepague.com.br>
2018-07-14 17:21:35 -0300

    Mapeamento das entidades Empresa e Funcionario. Criação dos Enums

---
+++ src/main/java/com/joaooliveira/pontointeligente/api/entities/Lancamento.java
@@ -0,0 +1,5 @@
+package com.joaooliveira.pontointeligente.api.entities;
+
+public class Lancamento {
+
+}
---
+++ src/main/java/com/joaooliveira/pontointeligente/api/enums/PerfilEnum.java
@@ -0,0 +1,7 @@
+package com.joaooliveira.pontointeligente.api.enums;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+
+}
